b7b8f9863286de29ceb1f1c43ff66a9697cfd625
Yogesh <yogeshpatil0811d@gmail.com>
2025-07-15 22:26:56 +0530

    Added RemoveDuplicatesFromSortedArray.cpp

---
+++ Array/RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+
+        int i = 0;
+        for (int j = 1; j < n; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter " << n << " sorted elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    Solution sol;
+    int k = sol.removeDuplicates(nums);
+
+    cout << "New length: " << k << "\n";
+    cout << "Array after removing duplicates: ";
+    for (int i = 0; i < k; i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
