c0144d02a5d1fff9023c30539162c86966480cf9
Yogesh <yogeshpatil0811d@gmail.com>
2025-07-31 23:42:01 +0530

    Inverted-Pyramid

---
+++ Patterns/invertedPyramid.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void inverted(int n)
+{
+
+    for (int i = n; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n = 5;
+    inverted(n);
+    return 0;
+}
