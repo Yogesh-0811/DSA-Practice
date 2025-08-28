8db4e20b8a8fac165fa85c344d4afc124ac1ece8
Yogesh <yogeshpatil0811d@gmail.com>
2025-08-28 12:30:35 +0530

    Leetcode 18

---
+++ Array/4Sum.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int left = j + 1, right = n - 1;
+                while (left < right) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        result.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+    int n;
+    cout<<"Enter the number of elements: ";
+    cin >> n; 
+
+    cout<<"Enter the elements: ";
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) cin >> nums[i];
+
+    cout<<"Enter the target: ";
+    int target;
+    cin>>target;
+
+    Solution sol;
+    vector<vector<int>> ans = sol.fourSum(nums, target);
+
+    for (auto &quad : ans) {
+        cout<<"[";
+        for (auto &x : quad){
+            cout << x << " ";
+        }
+        cout<<"]";
+        cout<<endl;
+    }
+
+    return 0;
+}
