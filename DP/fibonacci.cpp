e04405965fbbc17b3596840da2193dfa638fabd0
Yogesh <yogeshpatil0811d@gmail.com>
2025-09-09 18:38:42 +0530

    Fibonacci using DP

---
+++ DP/fibonacci.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int fib(int n){
+    if(n<=0) return n;
+    vector<int>dp(n+1);
+    dp[0]=0;dp[1]=1;
+    for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+    return dp[n];
+}
+
+int main(){
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+
+    cout<<"Fibonacci("<<n<<")="<<fib(n)<<endl;
+    return 0;
+}
